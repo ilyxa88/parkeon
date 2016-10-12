b1e279362f00f9a6c12675e9a29ca29800b04f6e
ilya yasiuk <iyasiuk@elinext.com>
2016-10-12 19:01:15 +0300

    hello

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<!doctype html> <html> <head> <meta charset="utf-8"> <title></title> <meta name="description" content=""> <meta name="viewport" content="width=device-width"> <!-- Place favicon.ico and apple-touch-icon.png in the root directory --> <link rel="stylesheet" href="styles/vendor.e5235dc9.css"> <link rel="stylesheet" href="styles/main.5b90bd7a.css"> </head> <body ng-app="parkeon2App"> <!--[if lte IE 8]>
+<!doctype html> <html> <head> <meta charset="utf-8"> <title></title> <meta name="description" content=""> <meta name="viewport" content="width=device-width"> <!-- Place favicon.ico and apple-touch-icon.png in the root directory --> <link rel="stylesheet" href="styles/vendor.e5235dc9.css"> <link rel="stylesheet" href="styles/main.5b90bd7a.css"> </head> <body ng-app="parkeon2App">
+<p>Hello world!!!</p>
+ <!--[if lte IE 8]>
       <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]--> <!-- With animation --> <!-- <div class="page {{ pageClass }}" ng-view></div> --> <!-- <div  ng-view></div> --> <div class="{{ page }} {{ pageClass }}" ui-view> <div class="page-loading-overlay"> <div class="loader-2"></div> </div> </div> <!-- Google Analytics: change UA-XXXXX-X to be your site's ID --> <script>!function(A,n,g,u,l,a,r){A.GoogleAnalyticsObject=l,A[l]=A[l]||function(){
        (A[l].q=A[l].q||[]).push(arguments)},A[l].l=+new Date,a=n.createElement(g),
